34d3e9951b3be054ca8abaf2788b0e4fe7599aa7
ahmadsanny2 <ahmadsanijabarulloh.02@gmail.com>
2025-12-31 02:04:01 +0700

    Add server checking endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
 import express from "express";
 import router from "./routes/api";
-import db from "./utils/database"
+import db from "./utils/database";
 import bodyParser from "body-parser";
 
 async function init() {
     try {
-        const result = await db()
+        const result = await db();
         console.log("Database Status:", result);
 
         const app = express();
 
-        app.use(bodyParser.json())
+        app.use(bodyParser.json());
 
         const PORT = 3000;
+
+        app.get("/", (req, res) => {
+            res.status(200).json({
+                message: "Server is running",
+                data: null,
+            });
+        });
 
         app.use("/api", router);
 
         app.listen(PORT, () => {
             console.log(`Server is running on port http://localhost:${PORT}`);
         });
-
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-init()
+init();
